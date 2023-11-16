5e563a91ad16f9094cbf5c7d7a20b2ac604e1db3
Olexand Fedoruk <alexandr.fedoruk1@gmail.com>
2023-11-16 14:54:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+
+function switchTheme(themeName) {
+    document.body.className = themeName;
+  }
+  
