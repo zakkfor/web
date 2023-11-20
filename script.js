0b0d8974ed336f7bfa7c6e80793b37fb88ffb0be
Olexand Fedoruk <alexandr.fedoruk1@gmail.com>
2023-11-20 22:03:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  let darkTheme = document.getElementById("dark-theme");
+  let lightTheme = document.getElementById("light-theme");
+  let normalTheme = document.getElementById("normal-theme");
 
+  darkTheme.addEventListener("click", function() {
+    document.body.style.backgroundColor = "#333";
+    document.body.style.color = "white";
+  });
 
-function switchTheme(themeName) {
-    document.body.className = themeName;
-  }
+  lightTheme.addEventListener("click", function() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "#333";
+  });
+
+  normalTheme.addEventListener("click", function() {
+    document.body.style.backgroundColor = "#808080";
+    document.body.style.color = "black";
+  });
   
+});
+ 
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
